501fd85de080632cc7d2a1076ed208fb91a35006
jwn122 <cat76122@hotmail.com>
2016-11-16 16:47:17 +0800

    1051115chang

---
+++ 1051115/上班心事＿S/practicegood.js
@@ -0,0 +1,10 @@
+var changeScene=0;
+function(decision){
+    if(changeScene==0){changeScene+=1;}
+    else if{(changeScene==1)?changeScene=2:changeScene=3;}
+    else if(changeScene==2){?changeScene=4:changeScene=5;}
+    else if(changeScene==3){?changeScene=6:changeScene=7;}
+    else if(changeScene==4){?changeScene=3:changeScene=5;}
+    else if(changeScene==7){changeScene==7?8:9;}
+        }
+document.getElementById("sceneimg").src="off life/"+changeScene+".jpg";
